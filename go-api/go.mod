875aef7682d778faae48e84dbc91a53a023431ee
Ahmed Samir <asnweery@gmail.com>
2021-10-27 18:43:42 +0200

    Fire up a basic server

--- go-api/go.mod
+++ go-api/go.mod
@@ -1,3 +1,11 @@
 module github.com/asamirr/Todo-Golang-Vue
 
 go 1.13
+
+require (
+	github.com/andybalholm/brotli v1.0.3 // indirect
+	github.com/gofiber/fiber/v2 v2.20.2
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/valyala/fasthttp v1.31.0 // indirect
+	golang.org/x/sys v0.0.0-20211025201205-69cdffdb9359 // indirect
+)
---
+++ go-api/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func main() {
+	app := fiber.New()
+	app.Use(logger.New())
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"message": "workingjdjincjsgg",
+		})
+
+	})
+	err := app.Listen(":8000")
+
+	if err != nil {
+		panic(err)
+	}
+}
